6f69d0bb854e8d20d71f9057f1fae596565e2a68
Michele <beschinmichele@gmail.com>
2019-02-07 14:17:18 +0100

    aggiunta funzione is_Admin (ritorna true se l'utente loggato è un amministratore)

--- Clinica Vet/DB_Access.php
+++ Clinica Vet/DB_Access.php
@@ -162,6 +162,14 @@ function insertImmaginiGalleria($path, $alt, $descrizione){
 	return $queryResult;
 
 }
+
+ function isAdminLogged($user){ //funzione che ritorna true sse c'è un amministratore loggato
+    $query="SELECT * FROM utente WHERE utente.Email=".$user." AND Admin!=0";
+    $queryResult = mysqli_query($this->connessione, $query) or die("impossibile stabilire se l'utente sia admin o meno");
+    if(mysqli_num_rows($queryResult) > 0)
+      return true;
+    return false;
+}
 }
 
 ?>
