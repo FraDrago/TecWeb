a28a20990621039a4fcf492307e0d321296fe2c5
Michele <beschinmichele@gmail.com>
2019-02-08 12:01:47 +0100

    annullato temporaneamente cambiamento db

--- Clinica Vet/file sql/clinica.sql
+++ Clinica Vet/file sql/clinica.sql
@@ -1,15 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
+﻿-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Creato il: Feb 08, 2019 alle 11:33
--- Versione del server: 10.1.37-MariaDB
--- Versione PHP: 7.3.0
+-- Host: localhost:3306
+-- Creato il: Feb 06, 2019 alle 16:50
+-- Versione del server: 5.7.25-0ubuntu0.18.04.2
+-- Versione PHP: 7.2.10-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,18 @@ SET time_zone = "+00:00";
 --
 -- Database: `clinica`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `animali`
+--
+
+CREATE TABLE `animali` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `utente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,9 +57,7 @@ INSERT INTO `galleria` (`id`, `Path`, `descrizione`, `alt`) VALUES
 (5, 'img/galleria/5.jpg', 'Mocio è stanco di lavorare', 'cane disteso su pavimento'),
 (6, 'img/galleria/7.jpg', 'Una bella giornata al sole', 'gatto in giardino al sole'),
 (7, 'img/galleria/6.jpg', 'Oggi non si esce di casa!', 'cane avvolto in una coperta'),
-(8, 'img/galleria/8.jpg', 'Un animale feroce!', 'piccolo cane con finta criniera di leone'),
-(0, 'img/galleria/2n74tp.jpg', 'is good', 'this'),
-(0, 'img/galleria/w2.png', 'tutto', 'hackero');
+(8, 'img/galleria/8.jpg', 'Un animale feroce!', 'piccolo cane con finta criniera di leone');
 
 -- --------------------------------------------------------
 
@@ -95,22 +103,16 @@ CREATE TABLE `prestazione` (
 -- Struttura della tabella `utente`
 --
 
-CREATE TABLE `utente` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `utente` (
+  `ID` int(0) NOT NULL AUTO_INCREMENT,
   `Name` tinytext NOT NULL,
   `Surname` tinytext NOT NULL,
   `Telefono` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `Admin` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `utente`
---
-
-INSERT INTO `utente` (`ID`, `Name`, `Surname`, `Telefono`, `Email`, `Password`, `Admin`) VALUES
-(0, 'gilberto', 'filÃ¨', '12345', 'file@unipd.it', '8c7dd922ad47494fc02c388e12c00eac', 1);
+  `Admin` tinyint(1) DEFAULT NULL,
+   PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
 
 -- --------------------------------------------------------
 
@@ -122,12 +124,108 @@ CREATE TABLE `visita` (
   `ID` int(11) NOT NULL,
   `DataOra` datetime NOT NULL,
   `Prestazione` int(11) NOT NULL,
+  `Animale` int(11) NOT NULL,
   `Utente` int(11) NOT NULL,
-  `Note` varchar(400) NOT NULL,
-  `tipo_animale` tinyint(1) DEFAULT NULL,
-  `approvation` tinyint(4) DEFAULT NULL
+  `Microchip` tinyint(1) NOT NULL,
+  `Note` varchar(400) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `animali`
+--
+ALTER TABLE `animali`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `animali_ibfk_1` (`utente`);
+
+--
+-- Indici per le tabelle `galleria`
+--
+ALTER TABLE `galleria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `orari`
+--
+ALTER TABLE `orari`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `prestazione`
+--
+ALTER TABLE `prestazione`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indici per le tabelle `visita`
+--
+ALTER TABLE `visita`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `visita_ibfk_1` (`Prestazione`),
+  ADD KEY `visita_ibfk_2` (`Animale`),
+  ADD KEY `visita_ibfk_3` (`Utente`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `animali`
+--
+ALTER TABLE `animali`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `galleria`
+--
+ALTER TABLE `galleria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT per la tabella `orari`
+--
+ALTER TABLE `orari`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT per la tabella `prestazione`
+--
+ALTER TABLE `prestazione`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `utente`
+--
+ALTER TABLE `utente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `visita`
+--
+ALTER TABLE `visita`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `animali`
+--
+ALTER TABLE `animali`
+  ADD CONSTRAINT `animali_ibfk_1` FOREIGN KEY (`utente`) REFERENCES `utente` (`ID`);
+
+--
+-- Limiti per la tabella `visita`
+--
+ALTER TABLE `visita`
+  ADD CONSTRAINT `visita_ibfk_1` FOREIGN KEY (`Prestazione`) REFERENCES `prestazione` (`ID`),
+  ADD CONSTRAINT `visita_ibfk_2` FOREIGN KEY (`Animale`) REFERENCES `animali` (`ID`),
+  ADD CONSTRAINT `visita_ibfk_3` FOREIGN KEY (`Utente`) REFERENCES `utente` (`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
