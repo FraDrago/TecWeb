2f3e3fbf434acee0ba3371b09ab87dd9332c9788
MartinaEO <campese.martina@gmail.com>
2019-02-10 00:46:38 +0100

    aggiunto mod

---
+++ Clinica Vet/mod.php
@@ -0,0 +1,29 @@
+<!--UPDATE table_name
+SET column1=value, column2=value2,...
+WHERE some_column=some_value  -->
+<?php
+require_once('DB_Access.php');
+$message = "";
+$result = true;
+if (isset($_POST["submit"]) && isset($_POST["alt"]) && !empty($_POST["alt"]) && isset($_POST["descrizione"]) && !empty($_POST["descrizione"])) {
+
+    $access = new DBAccess();
+    $connection = $access->openDBConnection();
+    if (!$connection) {
+        die("Errore nella connessione.");
+    } else {
+        if (!$access->updateImmagineGalleria($_POST["id"], $_POST["alt"], $_POST["descrizione"])) {
+            $result = false;
+            $message = "immagine non modificata";
+            die($message);
+        } else {
+            header("Location: galleria.php");
+        }
+    }
+
+} else {
+
+    $result = false;
+    $message = "Completa il form in modo corretto! Alcuni campi sono vuoti";
+}
+?>
