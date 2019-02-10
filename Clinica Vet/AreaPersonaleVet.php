5887f4169da3376a21dcc6f2c13e26d15fa7dee1
ValentinaBeni <45559597+ValentinaBeni@users.noreply.github.com>
2019-02-10 18:03:16 +0100

    font

--- Clinica Vet/AreaPersonaleVet.php
+++ Clinica Vet/AreaPersonaleVet.php
@@ -31,7 +31,7 @@ $pagina_attuale = 'AreaPersonaleVet.php'; ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/style2.css">
         <link rel="stylesheet" type="text/css" href="print.css" media="print">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
         <title>Ambulatorio Veterinario Archimedeo Torre</title>
     </head>
 
